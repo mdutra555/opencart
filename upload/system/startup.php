9c69af08cf292cfc64563d55a6eacd0831bb3c1b
Mauro Dutra <mdutra@mdisys.net>
2015-08-19 08:17:13 -0700

    Update startup.php

--- upload/system/startup.php
+++ upload/system/startup.php
@@ -93,3 +93,4 @@ require_once(modification(DIR_SYSTEM . 'engine/registry.php'));
 require_once(DIR_SYSTEM . 'helper/general.php');
 require_once(DIR_SYSTEM . 'helper/json.php');
 require_once(DIR_SYSTEM . 'helper/utf8.php');
+require_once(DIR_SYSTEM . 'helper/math_funcs.php');
